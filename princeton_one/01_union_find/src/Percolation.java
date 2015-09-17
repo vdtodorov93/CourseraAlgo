65580b84fa66d53f030496a98ce785823fe70a2f
vdtodorov93 <vdtodorov93@gmail.com>
2015-09-17 23:42:50 +0300

    Adding first task skeleton

---
+++ princeton_one/01_union_find/src/Percolation.java
@@ -0,0 +1,32 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    // create N-by-N grid, with all sites blocked
+    public Percolation(int N) {
+
+    }
+
+    // open site (row i, column j) if it is not open already
+    public void open(int i, int j){}
+
+    // is site (row i, column j) open?
+    public boolean isOpen(int i, int j) {
+        return false;
+    }
+
+    // is site (row i, column j) full?
+    public boolean isFull(int i, int j) {
+        return false;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("ASD");
+    } // test client (optional)
+}
